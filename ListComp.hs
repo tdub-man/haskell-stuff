4b855c7234e35788572172652ca793c70a692288
tmancil <mantanjon@gmail.com>
2016-07-24 19:34:47 -0400

    I was being stupid

--- ListComp.hs
+++ ListComp.hs
@@ -1,16 +1,29 @@
-module Main where
+module ListComp
+    ( fizzBuzz
+    , nth
+    , subList
+    ) where
 
-main :: IO ()
-main = putStrLn "Hello World"
-
-fbCond :: Integral a => a -> String
+fbCond :: (Integral a,Show a) => a -> String
 fbCond i
     | i `mod` 15 == 0 = "FIZZBUZZ"
     | i `mod` 3 == 0 = "FIZZ"
-    | otherwise = "BUZZ"
+    | i `mod` 5 == 0 = "BUZZ"
+    | otherwise = show i
 
 fizzBuzz :: [String]
 fizzBuzz = [fbCond i | i <- [1..], (i `mod` 3 == 0) || (i `mod` 5 == 0)]
 
-infBool :: [Bool]
-infBool = [even i | i <- [1..]]
+nth :: [a] -> Int -> Maybe a
+nth [] _ = Nothing
+nth (x:_) 0 = Just x
+nth (_:xs) n = nth xs (n - 1)
+
+subList :: Int -> [a] -> Maybe ([a],[a])
+subList _ [] = Nothing
+subList n xs
+  | length xs >= n = let
+      taken = take n xs
+      rest = drop n xs
+      in Just (taken,rest)
+  | otherwise = Just ([],xs)
