640f16d2998a1a5a356ca8d7c35c93a371dcb4d4
tmancil <mantanjon@gmail.com>
2016-07-25 11:57:08 -0400

    Removed old board model and can find next moves

--- PegBoard.hs
+++ PegBoard.hs
@@ -1,15 +1,16 @@
 module PegBoard
     ( Coord
     , Peg
-    , triangleNum
-    , pegCount
-    , openBoard
+    -- , triangleNum
+    , concatZip
+    , options
     , boardList
-    , boardLists
+    , openBoard
     , neighbor
     , validMove
+    , nextMoves
     ) where
-import Data.List(groupBy)
+-- import Data.List(groupBy)
 
 data Coord = Coord { xCoord :: Int, yCoord :: Int } deriving (Eq)
 data Peg = Peg { coord :: Coord, pegged :: Bool } deriving (Eq)
@@ -26,15 +27,10 @@ data BoardMoves = None
                 | PosLeft | PosRight
                 | ZedLeft | ZedRight
                 | NegLeft | NegRight deriving (Eq,Enum,Show)
--- type Peg = (Coord, Bool)
 type PegTriple = (Peg,Peg,Peg)
-type Board = [[Peg]]
 
-triangleNum :: Int -> Int
-triangleNum n = n * (n+1) `div` 2
-
-pegCount :: Board -> Int
-pegCount = triangleNum . length
+-- triangleNum :: Int -> Int
+-- triangleNum n = n * (n+1) `div` 2
 
 pegX :: Peg -> Int
 pegX = xCoord . coord
@@ -42,20 +38,24 @@ pegX = xCoord . coord
 pegY :: Peg -> Int
 pegY = yCoord . coord
 
--- function to randomly fill a Board with pegCount-1 pegs
-openBoard :: Coord -> Board -> Board
-openBoard (Coord x y) b = let
-  pegMatch (Peg c _) = if xCoord c == x && yCoord c == y
-    then Peg c False
-    else Peg c True
-  in map (map pegMatch) b
+concatZip :: [a] -> [a] -> [a]
+concatZip a = concat . zipWith (\x y -> [x,y]) a
+
+options :: [a] -> [a] -> [a] -> [(a,a,a)]
+options a b c = [ (x,y,z) | x <- a, y <- b, z <- c ]
+
+options' :: [a] -> [(a,a,a)]
+options' a = options a a a
 
 boardList :: Int -> [Peg]
 boardList n = [ Peg (Coord x y) False | x <- [1..n], y <- [1..x] ]
 
-boardLists :: Int -> Board
-boardLists n = groupBy sameRow $ boardList n where
-  sameRow p1 p2 = pegX p1 == pegX p2
+openBoard :: Coord -> [Peg] -> [Peg]
+openBoard (Coord x y) ps = let
+  pegMatch (Peg c _) = if xCoord c == x && yCoord c == y
+    then Peg c False
+    else Peg c True
+  in map pegMatch ps
 
 togglePeg :: Peg -> Peg
 togglePeg (Peg c b) = Peg c $ not b
@@ -84,3 +84,9 @@ validMove (a,b,c) = let
   in if neighAB == neighBC
     then neighAB
     else None
+
+nextMoves :: [Peg] -> [(Peg,Peg,Peg)]
+nextMoves ps = ops' where
+  ops = options' ps
+  validOps = map validMove ops
+  ops' = map fst . filter (\(_,mv) -> mv /= None) $ zip ops validOps
