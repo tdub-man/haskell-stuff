0fc951e05eabdd5201e7e21adb6fafb666897a89
tmancil <mantanjon@gmail.com>
2016-08-10 22:46:34 -0400

    Find the shortest game from a list of BoardLog

--- PegBoard.hs
+++ PegBoard.hs
@@ -10,6 +10,7 @@ module PegBoard
     , showBoard
     , showBoardLog
     , endWith
+    , shortestGame
     ) where
 import Data.List(partition,sortBy,intercalate)
 import ListHelpers(nPerms,moveXTo,groupWithNs)
@@ -147,3 +148,10 @@ playGameLog b = games where
 
 endWith :: Int -> [BoardLog] -> [BoardLog]
 endWith n = filter ((==n) . pegCount . _current)
+
+shorterGame :: BoardLog -> BoardLog -> BoardLog
+shorterGame a b = if gameLength a <= gameLength b then a else b where
+  gameLength = length . collectLog
+
+shortestGame :: [BoardLog] -> BoardLog
+shortestGame = foldr1 shorterGame
