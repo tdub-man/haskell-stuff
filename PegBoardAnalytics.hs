c43740bbf89d2b832dd23ac12229ca501b5adcb1
tmancil <mantanjon@gmail.com>
2016-08-11 05:10:17 -0400

    Find critical points on a board

--- PegBoardAnalytics.hs
+++ PegBoardAnalytics.hs
@@ -1,8 +1,10 @@
 module Main where
 
 import PegBoard
-import Data.List(sortBy,nubBy,group)
+import Data.List(sortBy,nubBy,group,groupBy)
 import Data.Function(on)
+import ListHelpers(middle)
+import MathHelpers(ceilDiv)
 
 -- ANALYTICS
 
@@ -30,16 +32,49 @@ numEndStates = numberOfPegCounts . endPegCount . uniqueEndStates where
   numberOf xs = (head xs,length xs)
   numberOfPegCounts = map numberOf . group
 
+-- CRITICAL POINTS
+-- Assuming full board
+-- Minimal unique starting coords,
+-- others can be found by roatating/flipping these
+
+rows :: Board -> [[Coord]]
+rows (Board ps _) = groupBy (\(Coord a _) (Coord b _) -> a == b) ps
+
+
+innerTriangle :: Board -> Board
+innerTriangle b = Board mids [] where
+  mids = concatMap middle . middle . rows $ b
+
+originShift :: Board -> Board
+originShift (Board ps _) = Board ps' [] where
+  ps' = map (\(Coord x y) -> Coord (x-2) (y-1)) ps
+
+topLeftRow :: Board -> [Coord]
+topLeftRow (Board ps _) = filter (\(Coord _ y) -> y == 1) ps
+
+critPoints' :: (Board,[Coord]) -> (Board,[Coord])
+critPoints' (Board [] [],cs) = (Board [] [],cs)
+critPoints' (b,cs) = critPoints' (b',cs') where
+  b' = originShift . innerTriangle $ b
+  tlRow = topLeftRow b
+  lenTLRow = length tlRow
+  n = lenTLRow `ceilDiv` 2
+  cs' = cs ++ take n tlRow
+
+critPoints :: Board -> [Coord]
+critPoints (Board [] _) = []
+critPoints b = cs where
+  (_,cs) = critPoints' (b,[])
+
 -- ALL GAMES ON 5 BOARD
 
--- Minimal unique starting coords
--- others can be found by roatating/flipping these
-tCoords :: [Coord]
-tCoords = [(Coord 1 1),(Coord 2 1),(Coord 3 1),(Coord 3 2)]
+-- tCoords :: [Coord]
+-- tCoords = [(Coord 1 1),(Coord 2 1),(Coord 3 1),(Coord 3 2)]
 
 tBoards :: [Board]
-tBoards = [ removePeg c b | c <- tCoords ] where
+tBoards = [ removePeg c b | c <- coords ] where
   b = makeBoard 5
+  coords = critPoints b
 
 tAllGames :: [[BoardLog]]
 tAllGames = map playGameLog tBoards
