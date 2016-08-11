b11dfba15a7f0e1943890716fda433d314cce378
tmancil <mantanjon@gmail.com>
2016-08-10 20:59:48 -0400

    Added logging board states

--- PegBoard.hs
+++ PegBoard.hs
@@ -2,7 +2,10 @@ module PegBoard
     ( Coord
     , Board
     , makeBoard
+    , removePeg
     , playGame
+    , playGameLog
+    , collectLog
     , showBoard
     ) where
 import Data.List(partition,sortBy,intercalate)
@@ -10,11 +13,14 @@ import ListHelpers(nPerms,moveXTo,groupWithNs)
 
 data Coord = Coord { _xCoord :: Int, _yCoord :: Int } deriving (Eq)
 data Board = Board { _pegs :: [Coord], _holes :: [Coord] } deriving (Eq)
+data BoardLog = BoardLog { _current :: Board, _history :: [Board] } deriving (Eq)
 
 instance Show Coord where
   show (Coord x y) = show (x,y)
 instance Show Board where
   show (Board ps hs) = "{ Pegs-" ++ show ps ++ " Holes-" ++ show hs ++ " }"
+instance Show BoardLog where
+  show = show . collectLog
 
 compareCoord :: Coord -> Coord -> Ordering
 compareCoord (Coord x1 y1) (Coord x2 y2) = let
@@ -36,6 +42,12 @@ showBoard (Board ps hs) = intercalate "\n" strs where
   spaces n = concat . replicate n $ " "
   spaceN = [acLength-1,acLength-2..1]
   strs = zipWith (\n cs -> spaces n ++ show cs) spaceN allCoord'
+
+changeBoard :: BoardLog -> Board -> BoardLog
+changeBoard (BoardLog b bs) x = BoardLog x (b:bs)
+
+collectLog :: BoardLog -> [Board]
+collectLog (BoardLog b bl) = reverse $ b:bl
 
 -- Pos[Left,Right] = Move along a line of positive slope
 -- Zed[Left,Right] = Move along a line of zero slope
@@ -86,9 +98,15 @@ nextMoves (Board ps hs) = trips' where
 movePegs :: (Coord,Coord,Coord) -> Board -> Board
 movePegs (a,b,c) = removePeg a . removePeg b . addPeg c
 
+movePegsLog :: (Coord,Coord,Coord) -> BoardLog -> BoardLog
+movePegsLog tri bLog = changeBoard bLog b where
+  b = movePegs tri $ _current bLog
+
 movePegsAll :: [(Coord,Coord,Coord)] -> Board -> [Board]
 movePegsAll mvs b = [ movePegs mv b | mv <- mvs ]
--- TODO : Implement logging
+
+movePegsAllLog :: [(Coord,Coord,Coord)] -> BoardLog -> [BoardLog]
+movePegsAllLog mvs bLog = [ movePegsLog mv bLog | mv <- mvs ]
 
 play :: ([Board],[Board]) -> ([Board],[Board])
 play ([],ended) = ([],ended)
@@ -99,6 +117,20 @@ play (playing,ended) = play (playing',ended') where
   ended' = ended ++ newlyEnded'
   playing' = concat [ movePegsAll mvs b | (b,mvs) <- playable ]
 
+playLog :: ([BoardLog],[BoardLog]) -> ([BoardLog],[BoardLog])
+playLog ([],ended) = ([],ended)
+playLog (playing,ended) = playLog (playing',ended') where
+  boardAndMoves = zip playing $ map (nextMoves . _current) playing
+  (newlyEnded,playable) = partition (\(_,m) -> null m) boardAndMoves
+  newlyEnded' = map fst newlyEnded
+  ended' = ended ++ newlyEnded'
+  playing' = concat [ movePegsAllLog mvs b | (b,mvs) <- playable ]
+
 playGame :: Board -> [Board]
 playGame b = endStates where
   (_,endStates) = play ([b],[])
+
+playGameLog :: Board -> [BoardLog]
+playGameLog b = games where
+  bLog = BoardLog b []
+  (_,games) = playLog ([bLog],[])
