b1a414b073ea96e494a5baa1b2d5c4bcefae315f
tmancil <mantanjon@gmail.com>
2016-08-17 03:11:54 -0400

    Kinda fixed Distance class

--- ClassHelpers.hs
+++ ClassHelpers.hs
@@ -1,15 +1,17 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances #-}
+
 module ClassHelpers
     ( Distance(dist,distMinMax,midElem)
     ) where
 
-class (Ord a) => Distance a where
-  dist :: (Num b) => a -> a -> b
-  distMinMax :: (Foldable t, Num b) => t a -> a -> b
+class (Ord n,Num n,Ord (a n)) => Distance a n where
+  dist :: a n -> a n -> n
+  distMinMax :: (Foldable t) => t (a n) -> a n -> n
   distMinMax xs = \x -> distMin x * distMax x where
     distMin = dist $ minimum xs
     distMax = dist $ maximum xs
   -- Finds middle-most element by distance
-  midElem :: (Foldable t) => t a -> a
+  midElem :: (Foldable t) => t (a n) -> a n
   midElem x = foldr1 maxDist x where
     dist' = distMinMax x
     maxDist a b = if max da db == da then a else b where
