4af384734bfa774f5a54c51eb8b814c0faef82f2
tmancil <mantanjon@gmail.com>
2016-10-27 19:09:13 -0400

    Fixed neighbor to get correct next moves

--- pegboard/app/Main.hs
+++ pegboard/app/Main.hs
@@ -33,28 +33,18 @@ concB' = concentricTrianglesExclusive b
 b' :: [Board]
 b' = collectLog . head . endWith 1 .  playGameLog $ b
 
-testPromptR :: Picture
-testPromptR = labeledPrompt where
-  board = renderBoard . makeBoard $ 5
-  rowLabels = pictures . offset 0 16 $ [ scale 0.1 0.1 $ Text (show n) | n <- [5,4..1] ]
-  rowLabels' = translate 0 (-4) . color white $ rowLabels
-  rowLines  = pictures [ Line [(0,16*n),(l+16,16*n)] | (n,l) <- zip [1..5] [0,8..32] ]
-  rowLines' = color white rowLines
-  labeledPrompt = pictures . offset 16 0 $ [rowLabels',rowLines',board]
-
-testPromptC :: Picture
-testPromptC = labeledPrompt where
-  board = renderBoard . makeBoard $ 5
-  colLabels = [ translate 20 36 . scale 0.1 0.1 . Text $ show n | n <- [5,4..1] ]
-  colLines = replicate 5 (Line [(0,0),(16,32)])
-  colLabelLines = [ pictures [lb,ln] | (lb,ln) <- zip colLabels colLines ]
-  colLabelLines' = color white . translate 104 8 . pictures . offset (-8) 16 $ colLabelLines
-  labeledPrompt = pictures [board,colLabelLines']
-
 main :: IO ()
+
+-- BOARD DISPLAYS
+
+-- main = displayBoard b
+-- main = displayBoards b'
+-- main = displayBoardsSquare b'
+--------------------------------------------------------------------------------
+
+-- TRANSFORMATIONS
+
 -- main = do
---   let printBoard = putStrLn . showBoard
---
 --   putStrLn "Base Board"
 --   printBoard b
 --   putStrLn "\nClockwise Rotation"
@@ -67,30 +57,16 @@ main :: IO ()
 --   printBoard . posFlip $ b
 --   putStrLn "\nNeg Flip"
 --   printBoard . negFlip $ b
---
+--------------------------------------------------------------------------------
+
+-- SYMMETRIES
+
 --   putStrLn "\nSymmetries:"
 --   mapM_ print . findSymmetries $ b
+--------------------------------------------------------------------------------
 
--- If negSymmetric, clockRotate == zedFlip and
---                  counterClockRotate == posFlip
-
--- main = displayBoard b
--- main = displayBoards b'
--- main = displayBoardsSquare b'
-
--- main = do
---   -- mapM_ (putStrLn . showBoard) concB
---   mapM_ (putStrLn . showBoard) concB'
---   print . last $ concB'
---
---   -- putStrLn . showBoard . zedFlip $ b
---   putStrLn . showBoard . clockRotate $ b
+-- INTERACTIVE
 
 -- main = displayInteractive b'
-
--- main = display
---   (InWindow "TestPrompt" (600,600) (0,0))
---   black
---   testPromptC
-
 main = promptSolve
+--------------------------------------------------------------------------------
--- pegboard/src/PegBoard.hs
+++ pegboard/src/PegBoard.hs
@@ -8,8 +8,8 @@ module PegBoard
     , showBoard
 
     , BoardMoves
-    , movePegs
     , nextMoves
+    , movePegs
     , movePegsAll
     ) where
 import Data.List(partition,sortBy,intercalate)
@@ -79,15 +79,21 @@ addPeg c (Board ps hs) = Board ps' hs' where
 pegCount :: Board -> Int
 pegCount = length . _pegs
 
--- b's relation to a
+-- Relation between a and b
+-- They are related in the pos direction if they are one row apart and in the same column
+-- They are related in the zed direction if they are in the same row and one column apart
+-- They are related in the neg direction if they are one row and one column apart
+-- Having a negative value for the relation represents
+-- coord1 being one row or column higher than coord2
 neighbor :: Coord -> Coord -> BoardMoves
-neighbor (Coord x1 y1) (Coord x2 y2) = case (abs (x2-x1),abs (y2-y1)) of
-    (-1,0)  -> Pos
-    (1,0)   -> Pos
+neighbor (Coord x1 y1) (Coord x2 y2) = let
+  in case (x2-x1,y2-y1) of
+    ( 1, 0) -> Pos
+    (-1, 0) -> Pos
+    ( 0, 1) -> Zed
+    ( 0,-1) -> Zed
+    ( 1, 1) -> Neg
     (-1,-1) -> Neg
-    (1,1)   -> Neg
-    (0,-1)  -> Zed
-    (0,1)   -> Zed
     _       -> None
 
 validMove :: (Coord,Coord,Coord) -> BoardMoves
@@ -95,14 +101,33 @@ validMove (a,b,c) = ab `bmAnd` bc where
   ab = neighbor a b
   bc = neighbor b c
 
+-- Creates a 3-tuple of two pegs and one hole
+-- Filters these tuples by whether they are valid moves
+-- First two coords are some permutation of the pegs
+-- The 3-tuple is a cartesian product of the previous permutations and the holes
+-- Filters the 3-tuples by whether they are valid moves
 nextMoves :: Board -> [(Coord,Coord,Coord)]
 nextMoves (Board ps hs) = trips' where
   pPerm = 2 `nPerms` ps
   trips = [ (a,b,c) | [a,b] <- pPerm, c <- hs ]
   trips' = filter (\tri -> validMove tri /= None) trips
 
+-- Takes the 3-tuple of two pegs and one hole
+-- Removes the pegs and adds the hole
 movePegs :: (Coord,Coord,Coord) -> Board -> Board
 movePegs (a,b,c) = removePeg a . removePeg b . addPeg c
 
 movePegsAll :: [(Coord,Coord,Coord)] -> Board -> [Board]
 movePegsAll mvs b = [ movePegs mv b | mv <- mvs ]
+
+-- In these definitions of nextMoves and movePegs,
+-- the move being made and what coordinates are pegs or holes
+-- are not explicitly specified
+-- We know from the order the triples are made that the
+-- first two coordinates are pegs and the remaining is a hole
+-- When filtering for valid moves, what don't keep the move that
+-- the triple represents, it is only implied by the data.
+-- Therefore, in movePegs, we know that
+-- "removePeg a . removePeg b . addPeg c" is a valid move, though
+-- a, b, and c aren't explicitly said to be pegs or holes, and their
+-- combination isn't explicitly stated as a certain move
